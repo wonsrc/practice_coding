4a253c21766001d8db344b321a80297522750b75
wonsrc <96459910+wonsrc@users.noreply.github.com>
2022-03-16 23:27:30 +0900

    jcb

---
+++ jcb/no36.js
@@ -0,0 +1,10 @@
+// let input = prompt();
+let input = 3;
+let output = "";
+
+for(let i = 1; i < 10; i++) {
+    output += input * i + ' ';
+    // output += ' ';
+}
+
+console.log(output);
---
+++ jcb/no37.js
@@ -0,0 +1,14 @@
+const arr = ["원범", "원범", "혜원", "혜원", "혜원", "혜원", "유진", "유진"];
+let res = {};
+let win = "";
+
+for(let i in arr) {
+    let key = arr[i];
+    res[key] = res[key] === undefined ? 1 : res[key] = res[key] + 1;
+}
+
+win = Object.keys(res).reduce(function(a, b) {
+    return res[a] > res[b] ? a : b
+});
+
+console.log(`${win}(이)가  총 ${res[win]} 표로 당선 되었습니다. `);
